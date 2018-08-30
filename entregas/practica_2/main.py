42210f53eef56677675f8c945724d23bc58ac35e
Lucas Rodriguez <ld.fennema@gmail.com>
2018-08-30 18:05:25 +0000

    Update main.py por pruebas

--- entregas/practica_2/main.py
+++ entregas/practica_2/main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     prg2 = Program("prg2.exe", [ASM.CPU(4), ASM.IO(), ASM.CPU(1)])
     prg3 = Program("prg3.exe", [ASM.CPU(3)])
 
-    batch = [prg1, prg2]#, prg3]
+    batch = [prg1, prg2, prg3]
     #  execute the program
     kernel.runBatch(batch)
 
