a3eb7dc863c9a0586943edfa8cf380ebdc1e7d16
Juan Carlos F. Stabile <jcfstabile@gmail.com>
2018-09-23 00:55:48 -0300

    Add stop, log level and state commands

--- entregas/practica_3/shell.py
+++ entregas/practica_3/shell.py
@@ -6,23 +6,35 @@ class shell():
 
     def com(kernel):
         log.logger.setLevel(60) # apago el log
+        HARDWARE.switchOff()
         _consolaCorriendo = True
         while (_consolaCorriendo):
             print("& ", end="")
             comandos = input().split()
             if comandos[0] == 'nolog':
                 log.logger.setLevel(60)
-            if comandos[0] == 'log':
+                
+            if comandos[0] == 'log' and len(comandos) == 2:
+                log.logger.setLevel(int(comandos[1]))
+            else:
                 log.logger.setLevel(0)
+
             if comandos[0] == 'start':
                 HARDWARE.switchOn()
-            if comandos[0] == 'halt':
+
+            if comandos[0] == 'halt' or comandos[0] == 'stop':
                 HARDWARE.switchOff()
+
             if comandos[0] == 'quit':
                 HARDWARE.switchOff()
                 _consolaCorriendo = False
+
+            if comandos[0] == 'state':
+                print(HARDWARE.cpu, HARDWARE.mmu)
+
             if comandos[0] == 'memory':
-                print(HARDWARE)
+                print(HARDWARE.memory)
+
             if comandos[0] == 'pcb':
                 if comandos[1] == 'path':
                     print('pcb: {}'.format(kernel.pcbTable.runningPCB.path))
