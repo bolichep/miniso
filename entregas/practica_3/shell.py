1dfe63fe335b4c5974b6ea0158d059237611b049
Juan Carlos F. Stabile <jcfstabile@gmail.com>
2018-09-26 20:35:23 -0300

    Add show memoryMap at memory command

--- entregas/practica_3/shell.py
+++ entregas/practica_3/shell.py
@@ -88,6 +88,7 @@ class shell():
                     print(kernel.readyQueue)
 
                 if comandos[0] == 'memory':
+                    print(kernel.loader.memoryMap)
                     print(HARDWARE.memory)
 
                 if comandos[0] == 'pcbtable':
