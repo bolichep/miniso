44aaf87e8273ee0ae87742198e122d2eba1ffb58
Juan Carlos F. Stabile <jcfstabile@gmail.com>
2018-10-13 12:47:55 -0300

    Add feature, multiple commands at shell's cli

--- entregas/practica_4/shell.py
+++ entregas/practica_4/shell.py
@@ -42,74 +42,84 @@ class shell():
                     print("& ", end="")
                     comandos = input().split()
 
-                if comandos[0] == 'ls':
-                    for f in shell.fs:
-                        print("{:<8} {}".format(f, shell.fs.get(f)))
+                while comandos:
 
-                if comandos[0] == 'save':
-                    shell.fs.update({comandos[1]: comandos[2]})
+                    if comandos[0] == 'ls':
+                        for f in shell.fs:
+                            print("{:<8} {}".format(f, shell.fs.get(f)))
 
-                if comandos[0] == 'load':
-                    _code = shell.fs.get(comandos[1])
-                    _name = comandos[1]
-                    print(_code)
+                    if comandos[0] == 'save':
+                        shell.fs.update({comandos[1]: comandos[2]})
 
-                if comandos[0] == 'run':
-                    kernel.run(Program(_name, [_code.split(",")]), 3 if len(comandos) < 2 else comandos[1])
+                    if comandos[0] == 'load':
+                        _code = shell.fs.get(comandos[1])
+                        _name = comandos[1]
+                        print(_code)
 
-                if comandos[0] == 'loaderreset':
-                    kernel.loader.memoryPos = 0
+                    if comandos[0] == 'run':
+                        kernel.run(Program(_name, [_code.split(",")]), 3 if len(comandos) < 2 else comandos[1])
 
-                if comandos[0] == 'help':
-                    print(shell.help_c)
+                    if comandos[0] == 'loaderreset':
+                        kernel.loader.memoryPos = 0
 
-                if comandos[0] == 'nolog':
-                    log.logger.setLevel(60)
+                    if comandos[0] == 'help':
+                        print(shell.help_c)
 
-                if comandos[0] == 'log' and len(comandos) == 2:
-                    log.logger.setLevel(int(comandos[1]))
-                else:
-                    log.logger.setLevel(0)
+                    if comandos[0] == 'nolog':
+                        log.logger.setLevel(60)
 
-                if comandos[0] == 'start':
-                    HARDWARE.switchOn()
+                    if comandos[0] == 'log' and len(comandos) == 2:
+                        log.logger.setLevel(int(comandos[1]))
+                    else:
+                        log.logger.setLevel(0)
 
-                if comandos[0] == 'halt' or comandos[0] == 'stop':
-                    HARDWARE.switchOff()
+                    if comandos[0] == 'start':
+                        HARDWARE.switchOn()
 
-                if comandos[0] == 'quit':
-                    HARDWARE.switchOff()
-                    _consolaCorriendo = False
+                    if comandos[0] == 'halt' or comandos[0] == 'stop':
+                        HARDWARE.switchOff()
 
-                if comandos[0] == 'reset':
-                    print(HARDWARE.setup(35))
+                    if comandos[0] == 'quit':
+                        HARDWARE.switchOff()
+                        _consolaCorriendo = False
 
-                if comandos[0] == 'state':
-                    print(HARDWARE.cpu, HARDWARE.mmu)
+                    if comandos[0] == 'reset':
+                        print(HARDWARE.setup(35))
 
-                if comandos[0] == 'iodevice':
-                    print(kernel.ioDeviceController)
+                    if comandos[0] == 'state':
+                        print(HARDWARE.cpu, HARDWARE.mmu)
 
-                if comandos[0] == 'readyqueue':
-                    print(kernel.scheduler.readyQueue)
+                    if comandos[0] == 'iodevice':
+                        print(kernel.ioDeviceController)
 
-                if comandos[0] == 'memory':
-                    print(HARDWARE.memory)
+                    if comandos[0] == 'readyqueue':
+                        print(kernel.scheduler.readyQueue)
 
-                if comandos[0] == 'pcbtable':
-                    if kernel.pcbTable.runningPCB != None:
-                        print("runningPCB: ", kernel.pcbTable.runningPCB,
-                                end="")
-                    print(kernel.pcbTable)
+                    if comandos[0] == 'memory':
+                        print(HARDWARE.memory)
 
-                if comandos[0] == 'tick':
-                    comandos.pop(0)
-                    count = 1
-                    if comandos:
-                        count = int(comandos[0])
-                    while count:
-                        HARDWARE.clock.tick(1)
-                        count -= 1
+                    if comandos[0] == 'pcbtable':
+                        if kernel.pcbTable.runningPCB != None:
+                            print("runningPCB: ", kernel.pcbTable.runningPCB,
+                                    end="")
+                        print(kernel.pcbTable)
+
+                    if comandos[0] == 'tick':
+                        comandos.pop(0)
+                        count = 1
+                        if comandos:
+                            count = int(comandos[0])
+                        while count:
+                            HARDWARE.clock.tick(1)
+                            count -= 1
+
+                    # done current command, see if we got next
+                    while comandos and comandos[0] is not ';':
+                        comandos.pop(0)
+                    # if first is ; remove it
+                    if comandos and comandos[0] is ';':
+                        comandos.pop(0)
+
 
             except KeyboardInterrupt:
                 print("\nTo exit, type: quit<Enter>\nor help to see commands")
