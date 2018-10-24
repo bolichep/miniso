0ec267f0c6d92f9f97925687c86409a3db053dab
Juan Carlos F. Stabile <jcfstabile@gmail.com>
2018-10-24 20:50:26 -0300

    Add frameSize keyword in Kernel instance call at main.py

--- entregas/practica_5/main.py
+++ entregas/practica_5/main.py
@@ -46,8 +46,8 @@ if __name__ == '__main__':
 
     ## new create the Operative System Kernel
     # "booteamos" el sistema operativo
-    kernel = Kernel(scheduler, 4)
-    sleep(1)
+    kernel = Kernel(scheduler, frameSize = 4)
+    # sleep(1)
 
     # Ahora vamos a intentar ejecutar 3 programas a la vez
     ##################
