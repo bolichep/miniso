1e2a9268b75ab1293c75a2cc215b4d505804e9e3
Juan Carlos F. Stabile <jcfstabile@gmail.com>
2018-11-08 21:48:29 -0300

    avance zero! main

--- entregas/practica_6/main.py
+++ entregas/practica_6/main.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 
     ## new create the Operative System Kernel
     # "booteamos" el sistema operativo
-    kernel = Kernel(HARDWARE,scheduler, frameSize = 8)
+    kernel = Kernel(HARDWARE,scheduler, frameSize = 4)
     # sleep(1)
 
     # Ahora vamos a intentar ejecutar 3 programas a la vez
