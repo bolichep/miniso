0a4d7ef51ff5c97e6feaff33579de49728f7527b
Juan Carlos F. Stabile <jcfstabile@gmail.com>
2018-12-06 08:24:54 -0300

    REWORK. shell.py

--- entregas/practica_6/shell.py
+++ entregas/practica_6/shell.py
@@ -5,12 +5,9 @@ import sys
 import readline
 
 
-class shell():
+class shell:
     help_c = """
-    nolog          : suprime la salida de log.logger
-    log            : habilita la salida de log.logger
     start          : enciende el hardware
-    halt           : detiene el hardware
     stop           : detiene el hardware
     quit           : sale del shell
     state          : muestra el estado del CPU y la MMU
@@ -21,22 +18,104 @@ class shell():
     ticktime n     : establece el tiempo en segundos de cada tick
     tick [n]       : envia n (o 1 por omision) tick de clock a los dispositivos subscriptos
     ls             : lista los programas salvados
-    save name code : salva el codigo en 'code' bajo el nombre 'name'
-                     ej: save uno CPU,IO,CPU,EXIT
-    load name      : carga el codigo bajo el nombre 'name' para ser usado en run
-    run            : corre el codigo previamente cargado con load
     """
 
+    def com(kernel):
+        ct = Thread(target = shell._com, kwargs = dict(kernel = kernel))
+        ct.start()
+        ct.join()
 
-    def _tick(count):
+    def _eval(args, kernel):
+        try:
+            print(eval(args[0]))
+        except NameError as ne:
+            print('NameError: {}'.format(ne))
+        except TypeError as te:
+            print('TypeError: {}'.format(te))
+
+    def _ls(args, kernel):
+        for f  in kernel.fileSystem.root:
+            print("{:<8} {}".format(f, kernel.fileSystem.root.get(f)))
+
+    def _run(args, kernel):
+        kernel.run(args[0], 3 if len(args) < 2 else int(args[1]))
+
+    def _ticktime(args, kernel):
+        HARDWARE.timeUnit = float(args[0])
+
+    def __tick(count):
         nbrTick = 0
         while count != nbrTick:
             nbrTick += 1
             HARDWARE.clock.tick(nbrTick)
 
-    def com(kernel):
-        ct = Thread(target = shell._com, kwargs = dict(kernel = kernel))
-        ct.start()
+    def _tick(args, kernel):
+        times = int(args[0])
+        thread_shell_tick = Thread(target = shell.__tick, kwargs = dict(count=times))
+        thread_shell_tick.start()
+        #thread_shell_tick.join()
+
+    def _help(args, kernel):
+        print(shell.help_c)
+
+    def _start(args, kernel):
+        HARDWARE.switchOn()
+
+    def _stop(args, kernel):
+        HARDWARE.switchOff()
+
+    def _quit(args, kernel):
+        HARDWARE.switchOff()
+        #_consolaCorriendo = False
+        return True
+
+    def _reset(args, kernel):
+        print(HARDWARE.setup(args[0]))
+
+    def _state(args, kernel):
+        print(HARDWARE.cpu, HARDWARE.mmu)
+
+    def _iodevice(args, kernel):
+        print(kernel.ioDeviceController)
+
+    def _readyqueue(args, kernel):
+        print(kernel.scheduler)
+
+    def _memory(args, kernel):
+        print(HARDWARE.memory)
+
+    def _pcbtable(args, kernel):
+        if kernel.pcbTable.runningPCB != None:
+            print("runningPCB:\n    ", kernel.pcbTable.runningPCB,
+                                end="")
+        print(kernel.pcbTable)
+
+    def _default(args, kernel):
+        if kernel.fileSystem.read(args[0]) != None:
+            kernel.run(args[0], 
+                    3 if len(args) < 2 
+                    else int(args[1]))
+
+    def _nothing(args, kernel):
+        pass
+            
+    commands = dict(
+            eval       = _eval,
+            ls         = _ls,
+            run        = _run,
+            ticktime   = _ticktime,
+            help       = _help,
+            start      = _start,
+            stop       = _stop,
+            reset      = _reset,
+            state      = _state,
+            iodevice   = _iodevice,
+            readyqueue = _readyqueue,
+            memory     = _memory,
+            pcbtable   = _pcbtable,
+            tick       = _tick,
+            quit       = _quit)
+    commands.update({'':_nothing})
 
     def _com(kernel):
         _consolaCorriendo = True
@@ -44,103 +123,21 @@ class shell():
         _name = []
         while (_consolaCorriendo):
             try:
-                comandos = []
-                while not comandos:
-                    comandos = input("& ").split()
+                script = []
+                while not script:
+                    script = input("& ").split(';')
 
-                while comandos:
-                    if comandos[0] == 'eval':
-                        try:
-                            print(eval(comandos[1]))
-                        except NameError as ne:
-                            print('NameError: {}'.format(ne))
-                        except TypeError as te:
-                            print('TypeError: {}'.format(te))
-
-
-                    if comandos[0] == 'ls':
-                        for f  in kernel.fileSystem.root:
-                            print("{:<8} {}".format(f, kernel.fileSystem.root.get(f)))
-
-                    if comandos[0] == 'save':
-                        kernel.fileSystem.write(comandos[1], 
-                                Program([comandos[2].split(",")]) )
-                        #shell.fs.update({comandos[1]: comandos[2]})
-
-                    if comandos[0] == 'load':
-                        _code = shell.fs.get(comandos[1])
-                        _name = comandos[1]
-                        print(_code)
-
-                    if comandos[0] == 'run':
-                        #kernel.fileSystem.write(_name, Program([_code.split(",")]) )
-                        kernel.run(comandos[1], 3 if len(comandos) < 2 else int(comandos[2]))
-
-                    if comandos[0] == 'ticktime':
-                        HARDWARE.timeUnit = float(comandos[1])
-
-                    if comandos[0] == 'loaderreset':
-                        kernel.loader.memoryPos = 0
-
-                    if comandos[0] == 'help':
-                        print(shell.help_c)
-
-                    if comandos[0] == 'nolog':
-                        log.logger.setLevel(60)
-
-                    if comandos[0] == 'log' and len(comandos) == 2:
-                        log.logger.setLevel(int(comandos[1]))
-                    else:
-                        log.logger.setLevel(0)
-
-                    if comandos[0] == 'start':
-                        HARDWARE.switchOn()
-
-                    if comandos[0] == 'halt' or comandos[0] == 'stop':
-                        HARDWARE.switchOff()
-
-                    if comandos[0] == 'quit':
-                        HARDWARE.switchOff()
-                        _consolaCorriendo = False
-
-                    if comandos[0] == 'reset':
-                        print(HARDWARE.setup(35))
-
-                    if comandos[0] == 'state':
-                        print(HARDWARE.cpu, HARDWARE.mmu)
-
-                    if comandos[0] == 'iodevice':
-                        print(kernel.ioDeviceController)
-
-                    if comandos[0] == 'readyqueue':
-                        print(kernel.scheduler)
-
-                    if comandos[0] == 'memory':
-                        print(HARDWARE.memory)
-
-                    if comandos[0] == 'pcbtable':
-                        if kernel.pcbTable.runningPCB != None:
-                            print("runningPCB: ", kernel.pcbTable.runningPCB,
-                                    end="")
-                        print(kernel.pcbTable)
-
-                    if comandos[0] == 'tick':
-                        comandos.pop(0)
-                        times = int(comandos[0])
-                        thread_shell_tick = Thread(target = shell._tick, kwargs = dict(count=times))
-                        thread_shell_tick.start()
-
-                    if kernel.fileSystem.read(comandos[0]) != None:
-                        kernel.run(comandos[0], 
-                                3 if len(comandos) < 2 
-                                else int(comandos[1]))
-
-                    # done current command, see if we got next
-                    while comandos and comandos[0] is not ';':
-                        comandos.pop(0)
-                    # if first is ; remove it
-                    if comandos and comandos[0] is ';':
-                        comandos.pop(0)
+                while script:
+                    line = script[0].split()
+                    script.pop(0)
+                    #print("line =", line, shell.commands)
+                    #print(len(line))
+                    if line: # and line[0] in shell.commands:
+                        if not line[0] in shell.commands:
+                            line = ['run'] + line
+                        cmd , args = line[0], line[1:] if len(line) > 1 else []
+                        #print("exec:", cmd, args)
+                        _consolaCorriendo = not shell.commands[cmd](args, kernel)
 
 
             except KeyboardInterrupt:
@@ -149,3 +146,5 @@ class shell():
             except (IndexError, AttributeError):
                 print("IndexError or AttributeError at {}".format(sys.exc_info()[-1].tb_lineno))
                 pass
+
+
